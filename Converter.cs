ca730476d79395649385e1f7f34fc91133007b99
kiragit01 <kiragithub@gmail.com>
2024-05-28 19:31:11 +0300

    Добавьте файлы проекта.

---
+++ Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP5
+{
+    class Converter
+    {
+        double usd;
+        double eur;
+        double rub;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public double ConvertToUsd(double byn) => byn * usd;
+        public double ConvertToEur(double byn) => byn * eur;
+        public double ConvertToRub(double byn) => byn * rub;
+
+        public double ConvertReversUsd(double count) => count / usd;
+        public double ConvertReversEur(double count) => count / eur;
+        public double ConvertReversRub(double count) => count / rub;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Converter converter = new Converter
+                (
+                usd: 0.30583, 
+                eur: 0.281437, 
+                rub: 27.93
+                );
+
+            Console.Write("Count BYN = ");
+            double count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{count} BYN = {converter.ConvertToUsd(count)} USD");
+            Console.WriteLine($"{count} BYN = {converter.ConvertToEur(count)} EUR");
+            Console.WriteLine($"{count} BYN = {converter.ConvertToRub(count)} RUB");
+
+
+            Console.Write("Count USD/EUR/RUB = ");
+            double countRevers = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{countRevers} USD = {converter.ConvertReversUsd(countRevers)} BYN");
+            Console.WriteLine($"{countRevers} EUR = {converter.ConvertReversEur(countRevers)} BYN");
+            Console.WriteLine($"{countRevers} RUB = {converter.ConvertReversRub(countRevers)} BYN");
+
+
+            Console.ReadKey();
+        }
+    }
+}
